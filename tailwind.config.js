2a6318b1493716d8d2608b20e6112efcc2df5ce8
Lavanya Katari <katarilavanya881@gmail.com>
2025-02-25 18:33:40 +0530

    🚑 fix: Update tailwind.config.js to remove unnecessary keyframes and animation properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,10 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// };
-
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      keyframes: {
-        moveRight: {
-          "0%": { transform: "translateX(-100px) skewX(5deg) skewY(2deg)" },
-          "100%": { transform: "translateX(100px) skewX(5deg) skewY(2deg)" },
-        },
-      },
-      animation: {
-        moveRight: "moveRight 2s infinite alternate ease-in-out",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
+
+
